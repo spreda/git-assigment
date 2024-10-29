94fbaa716baaa198ea8903a0d7c40bd593b7b373
Kirill Kamenev <ctktea@gmail.com>
2024-10-29 09:40:22 +0500

    Добавлены функции main() в каждый модуль

--- levinskiy_feature.py
+++ levinskiy_feature.py
@@ -1,2 +1,6 @@
-total = sum(range(1, 21))
-print("Сумма чисел от 1 до 20:", total)
+def main(n):
+    total = sum(range(1, n+1))
+    print(f"Сумма чисел от 1 до {n}:", total)
+
+if __name__ == '__main__':
+    main(20)
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    import skvortsov_feature
-    import levinskiy_feature
-    import camenev_feauture
+import skvortsov_feature
+import levinskiy_feature
+import camenev_feauture
 
-main()
+def main():
+    skvortsov_feature.main()
+    levinskiy_feature.main(20)
+    camenev_feauture.main(20)
+
+if __name__ == '__main__':
+    main()
