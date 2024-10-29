2013d07a18a040fe5b7ec819edada196cacbb050
Kirill Kamenev <ctktea@gmail.com>
2024-10-29 08:49:56 +0500

    Добавлен подсчет среднего чисел

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ def main():
     print("Hello, World!")
     total = sum(range(1, 11))
     print("Сумма чисел от 1 до 10:", total)
+    print("Среднее чисел от 1 до 10:", total/len(total))
 
 main()
